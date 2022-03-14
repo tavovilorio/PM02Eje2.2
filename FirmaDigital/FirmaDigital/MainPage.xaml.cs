c5017795d19038e44934ea45bc1a81197b9d467c
tavo <gustavolvilorio04@hotmail.com>
2022-03-13 18:13:18 -0600

    primer comentario

---
+++ FirmaDigital/FirmaDigital/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignaturePad.Forms;
+using Xamarin.Forms;
+
+namespace FirmaDigital
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonGuardar_Clicked(object sender, EventArgs e)
+        {
+           Stream image = await PadView.GetImageStreamAsync(SignatureImageFormat.jpg);
+        }
+
+        private void ButtonClear_Clicked(object sender, EventArgs e)
+        {
+            PadView.Clear();
+        }
+
+    }
+}
